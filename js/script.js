7992c21a49eb7d0d6d05b59bc56c89468720e7b5
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-06 12:48:39 +0100

    Optimizing code

--- js/script.js
+++ js/script.js
@@ -4,33 +4,41 @@ $(document).ready(function (){
     // query is saved from the input field
     var query = $('.search input').val();
     console.log(query);
+    // clear input field after the click
+    $('.search input').val('');
 
-    // ajax calling the themoviedb api for searching movies
-    $.ajax({
-      url: "https://api.themoviedb.org/3/search/movie",
-      method: "GET",
-      data: {
-        api_key: '6258744f8a6314eddb8961371f91076e',
-        query: query,
-        language: 'it-IT'
-      },
-      success: function(data, state) {
-        var movies = data.results;
-        console.log(movies);
-        searchPrintMovies(movies);
-      },
-      error: function(request, state, error) {
-        console.log(error);
-      }
-    });
+    searchMovies(query);
+
   });
 
 });
 
 
 // function
+// search the query using the api_key
+function searchMovies(query) {
+  // ajax calling themoviedb.org api for searching movies (max 20 displayed)
+  $.ajax({
+    url: "https://api.themoviedb.org/3/search/movie",
+    method: "GET",
+    data: {
+      api_key: '6258744f8a6314eddb8961371f91076e',
+      query: query,
+      language: 'it-IT'
+    },
+    success: function(data, state) {
+      var movies = data.results;
+      console.log(movies);
+      printMovies(movies);
+    },
+    error: function(request, state, error) {
+      console.log(error);
+    }
+  });
+}
+
 // search every movie in the array and print them in the html
-function searchPrintMovies(movies) {
+function printMovies(movies) {
   // clear movies field in the html
   $('.movies').text('');
   // handlebars init
@@ -57,6 +65,7 @@ function searchPrintMovies(movies) {
 // searching an empty string
 // if original title equals title show only one
 // start the search with enter key
+// add language selector
 
 // API Key (v3 auth)
 // 6258744f8a6314eddb8961371f91076e
