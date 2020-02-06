f5c487f3096bf9b26486a3c22c6dc21eefb259e5
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-06 12:08:15 +0100

    Added dynamic search

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 $(document).ready(function (){
 
-  // query
-  var query = 'star wars';
+  $(document).on('click', '.search button', function() {
+    // query is saved from the input field
+    var query = $('.search input').val();
+    console.log(query);
 
-  // ajax calling the themoviedb api for searching movies
-  $.ajax({
-    url: "https://api.themoviedb.org/3/search/movie",
-    method: "GET",
-    data: {
-      api_key: '6258744f8a6314eddb8961371f91076e',
-      query: query,
-      language: 'it-IT'
-    },
-    success: function(data, state) {
-      var movies = data.results;
-      console.log(movies);
-      searchPrintMovies(movies);
-    },
-    error: function(request, state, error) {
-      console.log(error);
-    }
+    // ajax calling the themoviedb api for searching movies
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/movie",
+      method: "GET",
+      data: {
+        api_key: '6258744f8a6314eddb8961371f91076e',
+        query: query,
+        language: 'it-IT'
+      },
+      success: function(data, state) {
+        var movies = data.results;
+        console.log(movies);
+        searchPrintMovies(movies);
+      },
+      error: function(request, state, error) {
+        console.log(error);
+      }
+    });
   });
 
 });
@@ -28,6 +31,8 @@ $(document).ready(function (){
 // function
 // search every movie in the array and print them in the html
 function searchPrintMovies(movies) {
+  // clear movies field in the html
+  $('.movies').text('');
   // handlebars init
   var source = $('#template').html();
   var template = Handlebars.compile(source);
@@ -48,6 +53,11 @@ function searchPrintMovies(movies) {
 
 
 // notes
+// to-do
+// searching an empty string
+// if original title equals title show only one
+// start the search with enter key
+
 // API Key (v3 auth)
 // 6258744f8a6314eddb8961371f91076e
 
